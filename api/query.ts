35732244adb7cdec80a6088603555030912ff1ed
Michal Niczyporuk <michal.niczyporuk@gmail.com>
2026-03-12 11:39:21 +0100

    chore(deps): update next to 15.2.8

--- api/query.ts
+++ api/query.ts
@@ -1,40 +1,58 @@
 "use server";
 
 export async function queryAPI(query: string = "") {
-  const token = process.env.DATALINKS_TOKEN;
-  const apiURL = process.env.DATALINKS_API_URL;
-  const dataset = process.env.DATALINKS_DATASET;
-  const useNaturalLanguageQuery = process.env.USE_NATURAL_LANGUAGE_QUERY === "1";
+    const token = process.env.DATALINKS_TOKEN;
+    const apiURL = process.env.DATALINKS_API_URL;
+    const datasetFlag = process.env.DATALINKS_DATASET;
+    const useNaturalLanguageQuery = process.env.USE_NATURAL_LANGUAGE_QUERY === "1";
 
-  if (!token) {
-    throw new Error("DATALINKS_TOKEN is not set");
-  }
+    if (!token) {
+        throw new Error("DATALINKS_TOKEN is not set");
+    }
 
-  if (!apiURL) {
-    throw new Error("DATALINKS_API_URL is not set");
-  }
+    if (!apiURL) {
+        throw new Error("DATALINKS_API_URL is not set");
+    }
 
-  if (!dataset) {
-    throw new Error("DATALINKS_DATASET is not set");
-  }
+    if (!datasetFlag) {
+        throw new Error("DATALINKS_DATASET is not set");
+    }
 
-  const url = `${apiURL}/data/${dataset?.toLowerCase()}/query`;
+    const url = `${apiURL}/query/`;
+    let coordinates = datasetFlag.split("/")
+    const username = coordinates[0];
+    const namespace = coordinates[1];
+    const dataset = coordinates[2];
+    let body: QueryRequest = {
+        username: username,
+        namespace: namespace,
+        dataset: dataset,
+    };
+    if (useNaturalLanguageQuery) {
+        body.naturalLanguageQuery = query;
+    } else {
+        body.query = query;
+    }
 
-  console.log("Querying for:", { url, query, useNaturalLanguageQuery });
+    console.log("Querying for:", {url, body});
 
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/text",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      query,
-      useNaturalLanguageQuery,
-    }),
-  });
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/text",
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(body),
+    });
 
-  const responseJson = await response.json();
+    const responseJson = await response.json();
+    return responseJson.data;
+}
 
-  return responseJson;
+interface QueryRequest {
+    username: string;
+    namespace: string;
+    dataset: string;
+    naturalLanguageQuery?: string;
+    query?: string;
 }
